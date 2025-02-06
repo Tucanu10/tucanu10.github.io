70088bf5603f9faf15dbaa4aef34d11209c4b35e
Tucanu <rockerulmaniac454@gmail.com>
2025-02-06 16:08:27 +0200

    Fix GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export", // Enables static export mode
+    reactStrictMode: true,
+    assetPrefix: "./",
+    basePath: "/tucanu10.github.io",
+    images: {
+      unoptimized: true
+    }
+  };
+  
+  export default nextConfig;
+  
